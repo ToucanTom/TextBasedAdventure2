a93fbd5eb294bc0546993eaca003cb45242fce58
Andrew Ripley <andrewripley99@yahoo.com>
2017-10-05 19:37:30 -0600

    Added block ball and paddle prefabs

---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    private Transform paddleTrans;
+
+    public float paddleHeight = -9;
+
+    void Start() {
+        paddleTrans = gameObject.GetComponent<Transform> ();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.y = paddleHeight;
+        mousePos.z = 0;
+        paddleTrans.position = mousePos;
+   
+    }
+}
