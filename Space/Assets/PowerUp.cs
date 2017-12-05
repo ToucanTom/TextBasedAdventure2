e87943d88d0c90b95c0f382bdcfb10905805978f
Andrew Ripley <andrewripley99@yahoo.com>
2017-12-04 18:27:11 -0700

    a lot

---
+++ Space/Assets/PowerUp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour {
+
+    public enum Type {damage, health, shield};
+    public Type powerUp;
+    
+  
+	// Use this for initialization
+	void Start () {
+	
+
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (powerUp)
+        {
+
+            case(Type.damage):
+                //increase attack
+                break;
+            case (Type.health):
+                //increase hp
+                break;
+            case (Type.shield):
+                //create a shield around player
+                break;
+        }
+    }
+
+}
